9c813ba7fea4ff98bd7ad4a16c1a412930185fee
happydavid226 <happydavid226@gmail.com>
2025-06-16 16:17:41 +0200

    Add files via upload
    Submit my Exam

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+Names : Happy David
+class : year 1 A
+email : happydavid226@gmail.com
+*/
+
+const express = require('express')
+const app = express()
+
+let  students = []
+
+
+function Student(name, grade){
+    this.name = name
+    this.grade = grade
+
+    let getDetails = () => {
+        let details = {
+            name : this.name,
+            grade : this.grade
+        };
+        return details
+    }
+}
+app.use(express.json())
+
+app.post('/students', (req, res) => {
+    let student = new Student(req.body.name, req.body.grade)
+    students.push(student)
+    res.send(student)
+})
+
+app.get('/students', (req, res) => {
+    res.send(students)
+})
+
+app.listen(3000, () => {
+    console.log ("app running on port 3000")
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`app running on port ${port} successfully`)
+})
+
+// email 
+//mwizstar@gmail.com
+
+
+
+
